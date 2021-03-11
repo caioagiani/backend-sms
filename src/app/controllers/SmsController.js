241d3e145630eadbc125ad9918572371f1f0e0cc
caioagiani <agianicaio@gmail.com>
2021-03-11 19:56:38 -0300

    feat: added more routers sms

--- src/app/controllers/SmsController.js
+++ src/app/controllers/SmsController.js
@@ -2,6 +2,33 @@ import mobizon from '../../services/mobizon';
 import { render } from '../views/SmsView';
 
 export default {
+  index: async (req, res) => {
+    const listSms = await mobizon.listSms({
+      criteria: {
+        from: '',
+      },
+      pagination: {
+        currentPage: '2',
+        pageSize: '50',
+      },
+      sort: {
+        campaignId: 'ASC',
+      },
+    });
+
+    return res.json(listSms);
+  },
+  show: async (req, res) => {
+    const { id } = req.params;
+
+    const getSms = await mobizon.getSms({
+      ids: [id],
+    });
+
+    console.log(id);
+
+    return res.json(getSms);
+  },
   store: async (req, res) => {
     const sendSms = await mobizon.sendSms(req.body);
 
